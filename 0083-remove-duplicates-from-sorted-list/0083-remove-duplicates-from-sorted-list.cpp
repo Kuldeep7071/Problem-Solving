4bd8a85121f7b24d69c67c21b2c0f254dd3134d9
Kuldeep7071 <ks707175@gmail.com>
2026-07-04 10:15:35 +0530

    Time: 0 ms (100%), Space: 16.2 MB (69.12%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == NULL)
+            return head;
+
+        ListNode* ptr = head;
+
+        while (ptr != NULL && ptr->next != NULL) {
+            if (ptr->val == ptr->next->val) {
+                ptr->next = ptr->next->next;
+            } else {
+                ptr = ptr->next;
+            }
+        }
+
+        return head;
+
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
