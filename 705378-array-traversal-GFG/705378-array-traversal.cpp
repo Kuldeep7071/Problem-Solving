eeeb000712b8b30473565a635ef1fa992de80ebf
Kuldeep7071 <ks707175@gmail.com>
2026-07-30 07:46:13 +0530

    Added solution - Time: O(size), Space: O(1)

---
+++ 705378-array-traversal-GFG/705378-array-traversal.cpp
@@ -0,0 +1,10 @@
+void arrayTraversal(int numbers[], int size) {
+    // Code here
+    for (int i=0; i<size; i++){
+        cout<< numbers[i]<< " ";
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
