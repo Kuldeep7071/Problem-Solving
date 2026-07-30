70377e9e09932c3dc5063a5387f28fbbaa4bc627
Kuldeep7071 <ks707175@gmail.com>
2026-07-30 07:42:11 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 703213-find-element-at-a-given-index-GFG/703213-find-element-at-a-given-index.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findElementAtIndex(self, i, arr):
+        # code here
+        return arr[i]
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
