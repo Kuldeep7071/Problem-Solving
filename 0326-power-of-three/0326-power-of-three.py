f64b625e52ff9d901814a44448bca4182989ce1c
Kuldeep7071 <ks707175@gmail.com>
2026-08-10 10:49:50 +0530

    Time: 10 ms (45.94%), Space: 19.3 MB (60.41%) - LeetHub - Time: O(log(n)), Space: O(1)

---
+++ 0326-power-of-three/0326-power-of-three.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        while n % 3 == 0:
+            n //= 3
+
+        if n == 1:
+            return True
+        else:
+            return False
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
