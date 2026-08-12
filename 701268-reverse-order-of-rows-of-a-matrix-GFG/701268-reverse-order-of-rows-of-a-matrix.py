b471afa8f882d2abf03c49df87dada6740825b2d
Kuldeep7071 <ks707175@gmail.com>
2026-08-12 10:53:28 +0530

    Added solution - Time: O(N), Space: O(1)

---
+++ 701268-reverse-order-of-rows-of-a-matrix-GFG/701268-reverse-order-of-rows-of-a-matrix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def interchangeRows(self, matrix: list[list[int]]) -> None:
+        # code here
+        return matrix.reverse()
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
